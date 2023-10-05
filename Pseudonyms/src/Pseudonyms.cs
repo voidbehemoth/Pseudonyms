8ab2e4b61519e8b7006a0a3c9d7334ca7663faba
VoidBehemoth <VoidBehemoth@gmail.com>
2023-10-04 19:48:43 -0600

    Add project files.

---
+++ Pseudonyms/src/Pseudonyms.cs
@@ -0,0 +1,103 @@
+﻿using Game.Interface;
+using HarmonyLib;
+using Server.Shared.Info;
+using Services;
+using SML;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Pseudonyms
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        private static string[] _configFileNames = new string[]
+        {
+            "FirstNames.txt",
+            "LastNames.txt",
+            "MonoNames.txt"
+        };
+
+        public static void Start()
+        {
+            Utils.Logger.Log("ain't no way");
+
+            GenerateDirectories();
+            GenerateFiles();
+        }
+
+        public static void GenerateDirectories()
+        {
+            string path = Path.GetDirectoryName(Application.dataPath) + "/SalemModLoader/ModFolders/Pseudonyms";
+
+            if (Directory.Exists(path)) return;
+
+            Directory.CreateDirectory(path);
+        }
+
+        public static void GenerateFiles()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            foreach (string fileName in _configFileNames)
+            {
+                if (File.Exists(Path.GetDirectoryName(Application.dataPath) + "/SalemModLoader/ModFolders/Pseudonyms/" + fileName)) continue;
+
+                string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith(fileName));
+                string result;
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+
+                File.WriteAllText(Path.GetDirectoryName(Application.dataPath) + "/SalemModLoader/ModFolders/Pseudonyms/" + fileName, result);
+            }
+        }
+    }
+
+
+    public static class ModInfo
+    {
+        public const string PLUGIN_GUID = "Pseudonyms";
+
+        public const string PLUGIN_NAME = "Pseudonyms";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+
+    
+    [HarmonyPatch(typeof(EndgameWrapupOverlayController))]
+    public class ListenerHandler
+    {
+        
+        [HarmonyPostfix]
+        [HarmonyPatch("InitializeListeners")]
+        public static void InitializeListeners()
+        {
+            Utils.Logger.Log("Initializing listeners...");
+            GameInfoObservation gameInfo = Service.Game.Sim.info.gameInfo;
+            gameInfo.OnDataChanged = (Action<GameInfo>)Delegate.Combine(gameInfo.OnDataChanged, new Action<GameInfo>(HandleGamePhaseChanged));
+        }
+
+        
+        [HarmonyPostfix]
+        [HarmonyPatch("OnDestroy")]
+        public static void OnDestroy()
+        {
+            Utils.Logger.Log("Disabling listeners...");
+            GameInfoObservation gameInfo = Service.Game.Sim.info.gameInfo;
+            gameInfo.OnDataChanged = (Action<GameInfo>)Delegate.Remove(gameInfo.OnDataChanged, new Action<GameInfo>(HandleGamePhaseChanged));
+        }
+
+        public static void HandleGamePhaseChanged(GameInfo gameInfo)
+        {
+            if (gameInfo.gamePhase != Server.Shared.State.GamePhase.PICK_NAMES || Pepper.IsMyInGameNameSet()) return;
+
+            if (ModSettings.GetBool("Random Names")) Utils.NameHelper.SetRandomName();
+        }
+    }
+}
---
+++ Pseudonyms/src/Utils.cs
@@ -0,0 +1,100 @@
+﻿using SalemModLoaderUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using SML;
+using Services;
+using Server.Shared.Messages;
+using Utils;
+using UnityEngine;
+
+namespace Pseudonyms.Utils
+{
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine("[" + Pseudonyms.ModInfo.PLUGIN_GUID + "] " + message);
+        }
+    }
+
+    public class NameHelper
+    {
+
+        private static string[] _cachedFirstNames;
+        private static string[] _cachedLastNames;
+        private static string[] _cachedMonoNames;
+
+        public static void SetRandomName()
+        {
+            Logger.Log("Setting random name");
+
+            bool useFirstAndLastNames = ModSettings.GetBool("Use First & Last Names");
+
+            string tempName, name;
+            do
+            {
+                tempName = GetRandomName(useFirstAndLastNames);
+                tempName.Trim();
+            } while (String.IsNullOrEmpty(tempName) || !ValidateName(tempName));
+            name = tempName;
+
+            PlayerNameMessage message = new PlayerNameMessage((byte)Pepper.GetMyPosition(), name);
+            Service.Game.Network.Send(message);
+            Storage.SetString(Storage.Key.GameName, name);
+            Storage.Save();
+
+            Logger.Log("Set name to " + name);
+
+            _cachedFirstNames = null; 
+            _cachedLastNames = null; 
+            _cachedMonoNames = null;
+        }
+
+        
+        public static string GetRandomName(bool useFirstAndLastNames)
+        {
+            string name = "John Doe";
+            try
+            {
+                if (useFirstAndLastNames && _cachedFirstNames == null) _cachedFirstNames = File.ReadAllLines(PathHelper.firstNamePath);
+                if (useFirstAndLastNames && _cachedLastNames == null) _cachedLastNames = File.ReadAllLines(PathHelper.lastNamePath);
+                if (!useFirstAndLastNames && _cachedMonoNames == null) _cachedMonoNames = File.ReadAllLines(PathHelper.monoNamePath);
+
+                System.Random random = new System.Random();
+
+                if (useFirstAndLastNames && (_cachedFirstNames.Length < 1 || _cachedLastNames.Length < 1) || !useFirstAndLastNames && _cachedMonoNames.Length < 1) return "John Doe";
+
+                name = useFirstAndLastNames ? _cachedFirstNames[random.Next(_cachedFirstNames.Length)] + " " + _cachedLastNames[random.Next(_cachedLastNames.Length)] : _cachedMonoNames[_cachedMonoNames.Length];
+            } catch (Exception e)
+            {
+                Logger.Log("error while getting random name: " + e);
+            }
+            
+            return name;
+        }
+
+        public static bool ValidateName(string name)
+        {
+            bool valid;
+            valid = (Regex.IsMatch(name, @"^[a-zA-Z ]+$") && name.Length <= 16);
+
+            string chatToCheck = name;
+            Service.Game.ProfanityFilter.FilterText(Service.Home.LocalizationService.GetUILanguageId(), ref chatToCheck, out var hasProfanity);
+            valid = valid && !hasProfanity;
+
+            return valid;
+        }
+    }
+
+    public class PathHelper
+    {
+        public static string firstNamePath = Path.GetDirectoryName(Application.dataPath) + "/SalemModLoader/ModFolders/Pseudonyms/FirstNames.txt";
+        public static string lastNamePath = Path.GetDirectoryName(Application.dataPath) + "/SalemModLoader/ModFolders/Pseudonyms/LastNames.txt";
+        public static string monoNamePath = Path.GetDirectoryName(Application.dataPath) + "/SalemModLoader/ModFolders/Pseudonyms/MonoNames.txt";
+    }
+}
